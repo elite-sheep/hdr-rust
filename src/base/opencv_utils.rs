ad185e116d1136c357b24666fc33229552016726
Yuchen Wang <wyc8094@gmail.com>
2020-03-25 23:21:40 +0800

    init_commit

---
+++ src/base/opencv_utils.rs
@@ -0,0 +1,10 @@
+/* Copyright 2020 Yuchen Wong */
+
+use opencv::core::{CV_8U, Mat, Scalar, Vec3b};
+use opencv::imgproc::{cvt_color, COLOR_RGB2GRAY};
+
+// This file contains some helper function of opencv
+// It is designed and implemented following c coding style.
+
+pub fn compute_mtb_image(src: &Mat, dst: &mut Mat) {
+}
