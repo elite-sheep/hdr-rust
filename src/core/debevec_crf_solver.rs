bebac52929449dbc431dabb31e40a8005ed605a5
Yuchen Wang <wyc8094@gmail.com>
2020-03-29 11:19:26 +0800

    almost_done

--- src/core/debevec_crf_solver.rs
+++ src/core/debevec_crf_solver.rs
@@ -1,18 +1,23 @@
 /* Copyright 2020 Yuchen Wong*/
 
-use opencv::core::{Mat}
-use opencv::prelude::{MatTrait, Vector};
+use opencv::core::{Mat, Vec3b, Vec3f};
+use opencv::prelude::{MatExprTrait, MatTrait, Vector};
 use opencv::types::VectorOfMat;
+use std::error::Error;
 
 #[path = "../base/math_utils.rs"] mod math_utils;
 
 pub fn solve(images: &VectorOfMat,
-             shutter_speeds: Vec<f32>,
+             shutter_speeds: &Vec<f32>,
+             lambda: f32,
              out_hdri: &mut Mat) -> Result<(), Box<dyn Error>> {
+
+    log::trace!("Debevec crf_solver started.");
+
     // generate weights with a hat function.
     let mut weights: [f32; 256] = [0.0; 256];
     for i in 0..256 {
-        weights[i] = math_utils::hat(i as f32, 0, 255);
+        weights[i] = math_utils::hat(i as f32, 0.0, 255.0);
     }
 
     let image_num = images.len();
@@ -21,36 +26,100 @@ pub fn solve(images: &VectorOfMat,
 
     // shape the output radiance map.
     unsafe {
-        out_hdri.new_rows_cols(rows, cols, opencv::core::CV_32FC3);
+        out_hdri.create_rows_cols(rows, cols, opencv::core::CV_32FC3)?;
     }
 
     // generate samples randomly.
-    let sample_num = rows * cols / image_num;
-    let samples_x: Vec<i32> = Vec::with_capacity(sample_num as usize);
-    let samples_y: Vec<i32> = Vec::with_capacity(sample_num as usize);
-    for i in 0..sample_num {
-        samples_x[i] = math_utils::gen_random_integer(0, cols);
-        samples_y[i] = math_utils::gen_random_integer(0, rows);
+    let sample_num = 2500;
+    let mut samples_x: Vec<i32> = Vec::with_capacity(sample_num as usize);
+    let mut samples_y: Vec<i32> = Vec::with_capacity(sample_num as usize);
+    for _i in 0..sample_num {
+        samples_x.push(math_utils::gen_random_integer(0, cols));
+        samples_y.push(math_utils::gen_random_integer(0, rows));
     }
 
     // Solve the radiance map for each channel
     for c in 0..3 {
-        solve_internal(images, shutter_speeds, &samples_x, &samples_y, c, out_hdri)?;
+        solve_internal(images, shutter_speeds, lambda, &weights, &samples_x, &samples_y, c, out_hdri)?;
     }
 
+    log::trace!("Debevec crf_solver ended..");
     Ok(())
 }
 
 fn solve_internal(images: &VectorOfMat,
                   shutter_speeds: &Vec<f32>,
+                  lambda: f32,
+                  weights: &[f32; 256],
                   samples_x: &Vec<i32>,
                   samples_y: &Vec<i32>,
                   channel: i32,
                   out_hdri: &mut Mat) -> Result<(), Box<dyn Error>> {
+
+    log::trace!("Solving Debevec CRF for channel {}.", channel);
+
     let rows = images.get(0)?.rows();
     let cols = images.get(0)?.cols();
     let sample_num: usize = samples_x.len();
+    let image_num: i32 = images.len() as i32;
 
-    let mut A: Mat = Mat::zeros(1+254+sample_num, 256+sample_num, CV_32FC1)?.as_mat();
-    let mut B: Mat = Mat::zeros(256+sample_num, 1, CV_32FC1)?.as_mat();
+    let total_sample_num = (sample_num as i32) * image_num;
+    let mut A: Mat = Mat::zeros(
+        1+254+total_sample_num, 256+total_sample_num, opencv::core::CV_32FC1)?.to_mat()?;
+    let mut B: Mat = Mat::zeros(
+        256+total_sample_num, 1, opencv::core::CV_32FC1)?.to_mat()?;
+
+    let mut l = 0;
+    for p in 0..image_num {
+        let cur_image: Mat = images.get(p as usize)?;
+        for i in 0..sample_num {
+            // log::trace!("{} {}", samples_y[i], samples_x[i]);
+            let z: Vec3b = *cur_image.at_2d::<Vec3b>(samples_y[i], samples_x[i])?;
+            *A.at_2d_mut::<f32>(l, z[channel as usize] as i32).unwrap() = 1.0 * weights[z[channel as usize] as usize];
+            *A.at_2d_mut::<f32>(l, (256 + i) as i32).unwrap() = -1.0 * weights[z[channel as usize] as usize];
+            *B.at_2d_mut::<f32>(l, 0).unwrap() = shutter_speeds[p as usize].ln() * weights[z[channel as usize] as usize];
+            l += 1;
+        }
+    }
+
+    *A.at_2d_mut::<f32>(l, 127).unwrap() = 1.0;
+    l += 1;
+
+    for i in 1..255 {
+        *A.at_2d_mut::<f32>(l, i-1).unwrap() = lambda * weights[i as usize];
+        *A.at_2d_mut::<f32>(l, i).unwrap() = -2.0 * lambda * weights[i as usize];
+        *A.at_2d_mut::<f32>(l, i+1).unwrap() = lambda * weights[i as usize];
+        l += 1;
+    }
+
+    log::trace!("Finishing setting up solver.");
+
+    let mut A_inv: Mat = Mat::default()?;
+    opencv::core::invert(&A, &mut A_inv, opencv::core::DECOMP_SVD)?;
+
+    log::trace!("Finishing solving SVD.");
+
+    let mut X: Mat = Mat::default()?;
+    opencv::core::multiply(&A_inv, &B, &mut X, 1.0, -1)?;
+
+    let mut g: [f32; 256] = [0.0; 256];
+    for i in 0..256 {
+        g[i as usize] = *X.at_2d::<f32>(i, 0).unwrap();
+    }
+
+    for row in 0..rows {
+        for col in 0..cols {
+            let mut sum_weight: f32 = 0.0;
+            let mut sum_radiance: f32 = 0.0;
+            for p in 0..image_num {
+                let z: u8 = *images.get(p as usize)?.at_2d::<u8>(row, col)?;
+                sum_weight += weights[z as usize];
+                sum_radiance += weights[z as usize] * (g[z as usize] - shutter_speeds[p as usize].ln());
+            }
+            out_hdri.at_2d_mut::<Vec3f>(row, col).unwrap()[channel as usize] = (sum_radiance / sum_weight).exp();
+        }
+    }
+
+    log::trace!("Finishing solving Debevec CRF for channel {}.", channel);
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -9,38 +9,40 @@ use opencv::types::{VectorOfi32, VectorOfMat};
 use std::error::Error;
 
 #[path = "./base/math_utils.rs"] mod math_utils;
+#[path = "./core/debevec_crf_solver.rs"] mod debevec_crf;
 #[path = "./core/mtb_image_alignment.rs"] mod mtb;
 
 fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
     log::trace!("HDR-Rust Starts.");
 
-    for i in 0..10 {
-        let j = math_utils::gen_random_integer(0, 100);
-        log::info!("{}", j);
-    }
-//    let mut images: VectorOfMat = VectorOfMat::new();
-//    let image1: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV+1.51.jpeg", 1)?;
-//    let image2: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV-1.82.jpeg", 1)?;
-//    let image3: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV-4.72.jpeg", 1)?;
-//    let image4: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV+1.18.jpeg", 1)?;
-//    let image5: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV+4.09.jpeg", 1)?;
-//    images.push(image1);
-//    images.push(image2);
-//    images.push(image3);
-//    images.push(image4);
-//    images.push(image5);
-//
-//    let mut out_aligned_images = VectorOfMat::new();
-//    mtb::align(&images, &mut out_aligned_images, 6)?;
-//
-//    log::trace!("Starting output images.");
-//    let img_write_types = VectorOfi32::with_capacity(0);
-//    imwrite("/home/yucwang/Desktop/lotus_1.jpeg", &out_aligned_images.get(0)?, &img_write_types)?;
-//    imwrite("/home/yucwang/Desktop/lotus_2.jpeg", &out_aligned_images.get(1)?, &img_write_types)?;
-//    imwrite("/home/yucwang/Desktop/lotus_3.jpeg", &out_aligned_images.get(2)?, &img_write_types)?;
-//    imwrite("/home/yucwang/Desktop/lotus_4.jpeg", &out_aligned_images.get(3)?, &img_write_types)?;
-//    imwrite("/home/yucwang/Desktop/lotus_5.jpeg", &out_aligned_images.get(4)?, &img_write_types)?;
+    let mut images: VectorOfMat = VectorOfMat::new();
+    let image1: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV-4.72.jpeg", 1)?;
+    let image2: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV-1.82.jpeg", 1)?;
+    let image3: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV+1.51.jpeg", 1)?;
+    let image4: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV+4.09.jpeg", 1)?;
+    images.push(image1);
+    images.push(image2);
+    images.push(image3);
+    images.push(image4);
+
+    log::trace!("Starting align images.");
+    let mut out_aligned_images = VectorOfMat::new();
+    mtb::align(&images, &mut out_aligned_images, 6)?;
+
+    log::trace!("Starting solving CRF.");
+    let mut out_hdri: Mat = Mat::default()?;
+    let mut shutter_speeds: Vec<f32> = Vec::new();
+    shutter_speeds.push(0.03333);
+    shutter_speeds.push(0.25);
+    shutter_speeds.push(2.5);
+    shutter_speeds.push(15.0);
+
+    debevec_crf::solve(&out_aligned_images, &shutter_speeds, 2.0, &mut out_hdri);
+
+    log::trace!("Starting output images.");
+    let options: VectorOfi32 = VectorOfi32::new();
+    imwrite("/home/yucwang/Desktop/lotus_hdr.exr", &out_hdri, &options);
 
     log::trace!("HDR-Rust ends.");
     Ok(())
