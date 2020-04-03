9bf245b86222655053f241877963edf1b521f86a
Wong Yuchen <wyc8094@gmail.com>
2020-04-03 20:58:39 +0800

    Yuchen/debevec hdr (#3)
    * introducing_random
    
    * init_commit
    
    * almost_done
    
    * fix_up_some_bugs
    
    * code_clean_up
    
    * fix_minor_error
    
    * fix_nan_error
    
    * fix_minor_bugs
    
    * change_penelty_to_0,5
    
    * fix_some_minor
    
    * fix_minor_again
    
    * add_startified_sampling
    
    * update_distribution
    
    * move_exr_output_as_a_function
    
    * fix_minor_error
    
    * fix_some_small_errors

---
+++ src/core/debevec_crf_solver.rs
@@ -0,0 +1,182 @@
+/* Copyright 2020 Yuchen Wong*/
+
+use opencv::core::{Mat, MatExprTrait, Vec3b, Vec3f};
+use opencv::prelude::*;
+use opencv::types::VectorOfMat;
+use std::error::Error;
+use std::time::SystemTime;
+
+#[path = "../base/math_utils.rs"] mod math_utils;
+#[path = "../base/opencv_utils.rs"] mod opencv_utils;
+
+use math_utils::{hat};
+use opencv_utils::matmul;
+
+pub fn solve(images: &VectorOfMat,
+             shutter_speeds: &Vec<f32>,
+             sample_num: i32,
+             lambda: f32,
+             out_hdri: &mut Mat) -> Result<(), Box<dyn Error>> {
+
+    log::trace!("Debevec crf_solver started.");
+
+    let start = SystemTime::now();
+
+    // generate weights with a hat function.
+    let mut weights: [f32; 256] = [0.0; 256];
+    for i in 0..256 {
+        weights[i] = hat(i as f32, 0.0, 255.0);
+    }
+
+    let rows: i32 = images.get(0)?.rows();
+    let cols: i32 = images.get(0)?.cols();
+
+    // shape the output radiance map.
+    unsafe {
+        out_hdri.create_rows_cols(rows, cols, opencv::core::CV_32FC3)?;
+    }
+
+//    let mut samples_x: Vec<i32> = Vec::with_capacity(sample_num as usize);
+//    let mut samples_y: Vec<i32> = Vec::with_capacity(sample_num as usize);
+//    for _i in 0..sample_num {
+//        samples_x.push(math_utils::gen_random_integer(0, cols));
+//        samples_y.push(math_utils::gen_random_integer(0, rows));
+//    }
+
+    // Solve the radiance map for each channel
+    for c in 0..3 {
+        solve_internal(images, shutter_speeds, lambda, &weights, sample_num, c as usize, out_hdri)?;
+    }
+
+    let duration = start.elapsed()?.as_secs();
+    log::trace!("Debevec crf_solver ended: {} secs.", duration);
+    Ok(())
+}
+
+fn solve_internal(images: &VectorOfMat,
+                  shutter_speeds: &Vec<f32>,
+                  lambda: f32,
+                  weights: &[f32; 256],
+                  sample_number: i32,
+                  channel: usize,
+                  out_hdri: &mut Mat) -> Result<(), Box<dyn Error>> {
+
+    log::trace!("Solving Debevec CRF for channel {}.", channel);
+
+    let mut samples_x: Vec<i32> = Vec::default();
+    let mut samples_y: Vec<i32> = Vec::default();
+    histogram_sampling(images, channel, sample_number, &mut samples_x, &mut samples_y)?;
+
+    let rows = images.get(0)?.rows();
+    let cols = images.get(0)?.cols();
+    let sample_num: usize = samples_x.len();
+    let image_num: i32 = images.len() as i32;
+
+    let total_sample_num = (sample_num as i32) * image_num;
+    let mut a: Mat = Mat::zeros(
+        1+254+total_sample_num, (256+sample_num) as i32, opencv::core::CV_32FC1)?.to_mat()?;
+    let mut b: Mat = Mat::zeros(
+        (1+254+total_sample_num) as i32, 1, opencv::core::CV_32FC1)?.to_mat()?;
+
+    let mut l = 0;
+    for p in 0..image_num {
+        let cur_image: Mat = images.get(p as usize)?;
+        for i in 0..sample_num {
+            // log::trace!("{} {}", samples_y[i], samples_x[i]);
+            let z: Vec3b = *cur_image.at_2d::<Vec3b>(samples_x[i], samples_y[i])?;
+            *a.at_2d_mut::<f32>(l, z[channel] as i32).unwrap() = 1.0 * weights[z[channel] as usize];
+            *a.at_2d_mut::<f32>(l, (256 + i) as i32).unwrap() = -1.0 * weights[z[channel] as usize];
+            *b.at_2d_mut::<f32>(l, 0).unwrap() = shutter_speeds[p as usize].ln() * weights[z[channel] as usize];
+            l += 1;
+        }
+    }
+
+    *a.at_2d_mut::<f32>(l, 127).unwrap() = 1.0;
+    l += 1;
+
+    for i in 1..255 {
+        *a.at_2d_mut::<f32>(l, i-1).unwrap() = lambda * weights[i as usize];
+        *a.at_2d_mut::<f32>(l, i).unwrap() = -2.0 * lambda * weights[i as usize];
+        *a.at_2d_mut::<f32>(l, i+1).unwrap() = lambda * weights[i as usize];
+        l += 1;
+    }
+
+    log::trace!("Finishing setting up solver.");
+
+    let mut a_inv: Mat = Mat::default()?;
+    opencv::core::invert(&a, &mut a_inv, opencv::core::DECOMP_SVD)?;
+
+    log::trace!("Finishing solving SVD.");
+
+    let mut x: Mat = Mat::default()?;
+    matmul(&a_inv, &b, opencv::core::CV_32FC1, &mut x)?;
+
+    let mut g: [f32; 256] = [0.0; 256];
+    for i in 0..256 {
+        g[i as usize] = *x.at_2d::<f32>(i, 0).unwrap();
+    }
+
+    log::trace!("Starting recovering for channel {}.", channel);
+    for row in 0..rows {
+        for col in 0..cols {
+            let mut sum_weight: f32 = 0.0;
+            let mut sum_radiance: f32 = 0.0;
+            for p in 0..image_num {
+                let z: Vec3b = *images.get(p as usize)?.at_2d::<Vec3b>(row, col).unwrap();
+                sum_weight += weights[z[channel] as usize];
+                sum_radiance += weights[z[channel] as usize] * (g[z[channel] as usize] - shutter_speeds[p as usize].ln());
+            }
+            out_hdri.at_2d_mut::<Vec3f>(row, col).unwrap()[channel] = (sum_radiance / (sum_weight+0.0001)).exp();
+        }
+    }
+
+    log::trace!("Finishing solving Debevec CRF for channel {}.", channel);
+    Ok(())
+}
+
+fn histogram_sampling(images: &VectorOfMat,
+                      channel: usize,
+                      sample_num: i32,
+                      out_sample_x: &mut Vec<i32>,
+                      out_sample_y: &mut Vec<i32>) -> Result<(), Box<dyn Error>> {
+    let bin_num = 8;
+    let image_num: usize = images.len();
+    let rows = images.get(0)?.rows();
+    let cols = images.get(0)?.cols();
+
+    let mut bin_x: Vec<Vec<i32>> = Vec::with_capacity(bin_num as usize);
+    let mut bin_y: Vec<Vec<i32>> = Vec::with_capacity(bin_num as usize);
+    let bin_size: i32 = 256 / bin_num;
+    for _i in 0..bin_num {
+        bin_x.push(Vec::with_capacity(bin_size as usize));
+        bin_y.push(Vec::with_capacity(bin_size as usize));
+    }
+
+    for i in 0..rows {
+        for j in 0..cols {
+            let mut sum_value: i32 = 0;
+            for k in 0..image_num {
+                sum_value += images.get(k)?.at_2d::<Vec3b>(i, j).unwrap()[channel] as i32;
+            }
+            sum_value /= image_num as i32;
+            bin_x[(sum_value / bin_size) as usize].push(i);
+            bin_y[(sum_value / bin_size) as usize].push(j);
+        }
+    }
+
+    let weights: [i32; 8] = [1, 2, 2, 3, 3, 2, 2, 1];
+
+    let samples_per_bin: i32 = sample_num / 16;
+    for i in 0..bin_num {
+        let cur_bin_size = bin_x[i as usize].len() as i32;
+        let cur_samples = weights[i as usize] * samples_per_bin;
+        for _j in 0..cur_samples {
+            let cur_index = math_utils::gen_random_integer(0, cur_bin_size);
+            out_sample_x.push(bin_x[i as usize][cur_index as usize]);
+            out_sample_y.push(bin_y[i as usize][cur_index as usize]);
+        }
+    }
+
+    log::trace!("Finishing sampling.");
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ extern crate pretty_env_logger;
 extern crate log;
 
 use opencv::core::Mat;
-use opencv::imgcodecs::{imread, imwrite};
+use opencv::imgcodecs::{imread};
 use opencv::prelude::Vector;
-use opencv::types::{VectorOfi32, VectorOfMat};
+use opencv::types::{VectorOfMat};
 
 use std::error::Error;
 
+#[path = "./base/math_utils.rs"] mod math_utils;
+#[path = "./base/opencv_utils.rs"] mod opencv_utils;
+#[path = "./core/debevec_crf_solver.rs"] mod debevec_crf;
 #[path = "./core/mtb_image_alignment.rs"] mod mtb;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -15,27 +18,33 @@ fn main() -> Result<(), Box<dyn Error>> {
     log::trace!("HDR-Rust Starts.");
 
     let mut images: VectorOfMat = VectorOfMat::new();
-    let image1: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV+1.51.jpeg", 1)?;
-    let image2: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV-1.82.jpeg", 1)?;
-    let image3: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV-4.72.jpeg", 1)?;
-    let image4: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV+1.18.jpeg", 1)?;
-    let image5: Mat = imread("/home/yucwang/Pictures/test_pictures/hdr_test_cases/1440px-StLouisArchMultExpEV+4.09.jpeg", 1)?;
+    let image1: Mat = imread("/home/yucwang/I_love_hefei_50/IMG_20200402_183757.jpg", 1)?;
+    let image2: Mat = imread("/home/yucwang/I_love_hefei_50/IMG_20200402_183928.jpg", 1)?;
+    let image3: Mat = imread("/home/yucwang/I_love_hefei_50/IMG_20200402_184101.jpg", 1)?;
+    // let image4: Mat = imread("/home/yucwang/Pictures/i_love_hefei/i_love_hefei_3.jpg", 1)?;
     images.push(image1);
     images.push(image2);
     images.push(image3);
-    images.push(image4);
-    images.push(image5);
+    // images.push(image4);
 
+    log::trace!("Starting align images.");
     let mut out_aligned_images = VectorOfMat::new();
-    mtb::align(&images, &mut out_aligned_images, 6)?;
+    mtb::align(&images, &mut out_aligned_images, 8).unwrap();
+
+    images.clear();
+
+    log::trace!("Starting solving CRF.");
+    let mut out_hdri: Mat = Mat::default()?;
+    let mut shutter_speeds: Vec<f32> = Vec::new();
+    shutter_speeds.push(0.0182);
+    shutter_speeds.push(0.0667);
+    shutter_speeds.push(1.0);
+    // shutter_speeds.push(2.0);
+
+    debevec_crf::solve(&out_aligned_images, &shutter_speeds, 512, 0.7, &mut out_hdri).unwrap();
 
     log::trace!("Starting output images.");
-    let img_write_types = VectorOfi32::with_capacity(0);
-    imwrite("/home/yucwang/Desktop/lotus_1.jpeg", &out_aligned_images.get(0)?, &img_write_types)?;
-    imwrite("/home/yucwang/Desktop/lotus_2.jpeg", &out_aligned_images.get(1)?, &img_write_types)?;
-    imwrite("/home/yucwang/Desktop/lotus_3.jpeg", &out_aligned_images.get(2)?, &img_write_types)?;
-    imwrite("/home/yucwang/Desktop/lotus_4.jpeg", &out_aligned_images.get(3)?, &img_write_types)?;
-    imwrite("/home/yucwang/Desktop/lotus_5.jpeg", &out_aligned_images.get(4)?, &img_write_types)?;
+    opencv_utils::save_exr_with_default(&std::string::String::from("/home/yucwang/Desktop/i_love_hefei_hdr.exr"), &out_hdri)?;
 
     log::trace!("HDR-Rust ends.");
     Ok(())
